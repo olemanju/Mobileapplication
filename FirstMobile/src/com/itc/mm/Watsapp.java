8495b7224261676e1fedc97fb49c6aac58b8d43e
Manju <manju.ole23@gmail.com>
2017-10-10 20:55:31 +0530

    MOving first mobile project to git

---
+++ FirstMobile/src/com/itc/mm/Watsapp.java
@@ -0,0 +1,24 @@
+package com.itc.mm;
+
+
+
+public class Watsapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("mnajunatha");
+		String name="manju";
+		if(name.equals("manju"))
+		{
+			System.out.println("Pasws");
+		}
+		else
+		{
+			System.out.println("failed");
+		}
+		
+
+	}
+
+}
